06d569d29052be1d94049ee346950cca3ec0c667
Patrick NANFOUET <nanfluetpatrick5@gmail.com>
2022-10-17 15:26:29 -0700

    for

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 int i;
 for (i = 10; i < 20; i++)
 {
-putchar((i % 10) + '0');      
-if (i != 19)	
-{	  
+putchar((i % 10) + '0');
+if (i != 19)
+{
 putchar(',');
 putchar(' ');
-}      
-}  
+}
+}
 putchar('\n');
-return (0); 
+return (0);
 }
