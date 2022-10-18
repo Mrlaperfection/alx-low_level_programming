46ca129230d2829dd36fe8de158e1fc456dde153
Patrick NANFOUET <nanfluetpatrick5@gmail.com>
2022-10-18 16:36:05 -0700

    qsdqs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
+/**
+
+ * print_to_98 - Entry point
+
+ * 
+
+ * Description: 'the program's description'
+
+ * 
+
+ * Return: Always 0 (Success)
+
+ */
 void print_to_98(int n)
 {
 int i;
