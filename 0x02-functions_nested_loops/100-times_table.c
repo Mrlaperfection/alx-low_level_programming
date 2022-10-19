62bfbb9abb2a834930631f6ba13a35071dd89f36
Patrick NANFOUET <nanfluetpatrick5@gmail.com>
2022-10-18 17:13:50 -0700

    qsdqs

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,8 @@ if (j == 0)
 {
 _putchar('0');
 }
-else if (prod <= 9){
+else if (prod <= 9)
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
@@ -43,7 +44,6 @@ _putchar('0' + (prod / 10));
 _putchar('0' + (prod % 10));
 }
 }
-_putchar('\n');
 }
 }
 }
